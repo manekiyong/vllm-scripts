2d0f9ea8e08019db884deb23e4491e29a022b298
manekiyong <59855081+manekiyong@users.noreply.github.com>
2025-11-14 15:32:30 +0800

    fix: head no longer an arg

--- run_cluster.sh
+++ run_cluster.sh
@@ -19,7 +19,7 @@ trap cleanup EXIT
 
 # Command setup for head or worker node
 RAY_START_CMD="ray start --block"
-if [ "${NODE_TYPE}" == "--head" ]; then
+if [ "${NODE_TYPE}" == "head" ]; then
     RAY_START_CMD+=" --head --port=6379"
 else
     RAY_START_CMD+=" --address=${HEAD_NODE_ADDRESS}:6379 --node-ip-address=${SELF_NODE_ADDRESS}"
