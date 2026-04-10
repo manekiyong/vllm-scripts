f9771df25f6dc2839acf4a8236838bd46e6c13d1
normalwalrus <ianchanjiajun@gmail.com>
2026-04-10 16:56:36 +0800

    fix example & gemma4 script

--- vllm/gemma4/gemma-4-26B-A4B-it.sh
+++ vllm/gemma4/gemma-4-26B-A4B-it.sh
@@ -1,5 +1,10 @@
-vllm serve google/gemma-4-26B-A4B-it \
-  --max-model-len 131072 \
+vllm serve /models/huggingface/google/gemma-4-26B-A4B-it \
+  --served_model_name gemma-4-26B-A4B-it \
+  --max-model-len 65536 \
   --enable-auto-tool-choice \
   --reasoning-parser gemma4 \
-  --tool-call-parser gemma4
+  --tool-call-parser gemma4 \
+  --tensor_parallel_size 2 \
+  --seed 0 \
+  --max_num_seqs 8 \
+  --gpu_memory_utilization 0.88
